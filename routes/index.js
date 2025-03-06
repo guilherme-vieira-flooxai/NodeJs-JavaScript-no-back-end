05d3a48355124beddbd8239441cc315d966a5663
guilherme-vieira-flooxai <guilherme.vieira.flooxai@gmail.com>
2025-03-06 09:55:15 -0300

    feat: receiving data via POST and listing NeDB bank user

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ module.exports = app=>{
 
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>oi</h1>');
+        res.end('<h1>Salve!</h1>');
     
     });
 
